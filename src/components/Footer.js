54a1870429a269347220554d386a1776fa5caea3
vinay <vinay63030@gmail>
2024-12-28 10:24:45 +0530

    Your message describing the changes

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer style={{ padding: '10px', backgroundColor: '#333', color: '#fff', marginTop: '20px' }}>
-    <p>© 2024 Shoppin. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
